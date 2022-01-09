e3985c9ac21bc3c5be5b0772e34c5818d328fcf8
Ren Wang <iamwrm@gmail.com>
2022-01-10 02:13:38 +0800

    max 256

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import sys
 
 def print_id(args):
+    dim_length = 6
     args = [int(i) for i in args]
     idx = 0
     for i,v in enumerate(args):
-        idx +=v* (8**i)
+        idx +=v* (dim_length**i)
     print(f"job_id: {idx}")
 
 
