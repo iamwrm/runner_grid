0c0a819083672cd54e579e1383a6c2a7fab8615d
Ren Wang <iamwrm@gmail.com>
2022-01-12 13:14:24 +0800

    try 4 digits

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 import sys
 
-def print_id(args):
-    dim_length = 6
+def get_id(args):
+    assert(len(args) == 4)
+    dim_length = 4
     args = [int(i) for i in args]
     idx = 0
-    for i,v in enumerate(args):
+    for i,v in enumerate(args[::-1]):
         idx +=v* (dim_length**i)
-    print(f"job_id: {idx}")
+    return idx
 
 
 if __name__ == '__main__':
     provided_args = sys.argv[1:]
-    print_id(provided_args)
+
+    idx= get_id(provided_args)
+
+    print(f"job_id: {idx}")
