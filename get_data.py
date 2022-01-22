bb6158c87a4a71fe4f1ae43a0f40bae062faaa74
Ren Wang <iamwrm@gmail.com>
2022-01-22 08:03:49 +0000

    try tushare

---
+++ get_data.py
@@ -0,0 +1,26 @@
+import tushare as ts
+
+
+def get_stock_info(ts_api, ts_code):
+    start_date = '2019-01-01'
+    end_date = '2020-01-01'
+    df = ts_api.daily(
+        ts_code=ts_code,
+        start_date=start_date,
+        end_date=end_date)
+    return df
+
+
+def create_tushare():
+    with open('./env') as f:
+        lines = f.readlines()
+        ts_token = lines[0].strip()
+        ts_api = ts.pro_api(ts_token)
+        return ts_api
+
+
+if __name__ == '__main__':
+    ts_code = '000001.SZ'  # 第一号股票，平安
+    ts_api = create_tushare()
+    df = get_stock_info(ts_api, ts_code)
+    print(df)
--- run.py
+++ run.py
@@ -1,18 +1,29 @@
 import sys
+import get_data
+
 
 def get_id(args):
     assert(len(args) == 4)
     dim_length = 4
     args = [int(i) for i in args]
     idx = 0
-    for i,v in enumerate(args[::-1]):
-        idx +=v* (dim_length**i)
+    for i, v in enumerate(args[::-1]):
+        idx += v * (dim_length**i)
     return idx
+
+# 20-> 000020.SZ
+def get_stock_code(idx: int):
+    return (6-len(str(idx))) * '0' + str(idx) + '.SZ'
 
 
 if __name__ == '__main__':
     provided_args = sys.argv[1:]
 
-    idx= get_id(provided_args)
+    idx = get_id(provided_args)
+    ts_code = get_stock_code(idx)
 
     print(f"job_id: {idx}")
+    print(f"ts_code: {ts_code}")
+
+    df = get_data.get_stock_info(ts_code)
+    print(df)
